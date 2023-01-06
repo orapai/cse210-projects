e56477c5754288ae86cf53f68bfff30666aa9856
orapai <ivy.orapa.amodia@gmail.com>
2023-01-05 21:43:56 -0800

    Finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string replay = "yes";
+        string play = "yes";
+
+        while (replay == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+            Console.WriteLine($"What is the magice number? {number}");
+            int guessNumber = -1;
+            int gameCount = 0;
+            while (number != guessNumber)
+            {
+                Console.Write("What is your guess? ");
+                string answer = Console.ReadLine();
+                guessNumber = int.Parse(answer);
+                gameCount = gameCount + 1;
+                if (number > guessNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (number < guessNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Yhay!");
+                    if (gameCount > 1)
+                    {
+                        Console.WriteLine($"You've guessed it in just {gameCount} tries.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You've guessed it in just {gameCount} try.");
+                    }
+
+                    Console.Write("Would you like to play again? ");
+                    play = Console.ReadLine();
+                    if (play.ToLower() == "yes") 
+                    {
+                        replay = play;
+                    }
+                    else 
+                    {
+                        play = "";
+                        replay = play;
+                        Console.WriteLine("Thanks for playing!");
+                    }
+                }
+            }
+        }
     }
 }
