5bc46b323af2d13a7cde93b28987e252c6baecfe
orapai <ivy.orapa.amodia@gmail.com>
2023-01-07 16:23:08 +0800

    Finished Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        DisplayResult(name, num);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number:");
+        string number = Console.ReadLine();
+        int x = int.Parse(number);
+        x = SquareNumber(x);
+        return x;
+    }
+    static int SquareNumber(int y)
+    {
+        int squared = y * y;
+        Console.WriteLine(squared);
+        return squared;
+    }
+    static void DisplayResult(string x, int y)
+    {
+        Console.WriteLine($"{x}, the square of your number is {y}");
     }
 }
