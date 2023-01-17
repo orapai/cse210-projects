b6816cd4eea093beb460e1040333d93318c6f545
orapai <ivy.orapa.amodia@gmail.com>
2023-01-16 23:52:45 -0800

    made changes

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction number1 = new Fraction();
+        Console.WriteLine(number1.GetFractionString());
+        Console.WriteLine(number1.GetTop());
+        Fraction number2 = new Fraction(5);
+        Console.WriteLine(number2.GetFractionString());
+        Console.WriteLine(number2.GetTop());
+        Fraction number3 = new Fraction(3,4);
+        Console.WriteLine(number3.GetFractionString());
+        Console.WriteLine(number3.GetDecimalValue());
+        Fraction number4 = new Fraction(1,3);
+        Console.WriteLine(number4.GetFractionString());
+        Console.WriteLine(number4.GetDecimalValue());
     }
 }
