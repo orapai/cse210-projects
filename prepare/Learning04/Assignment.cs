e6351e0fb4f451ed50cd29fcbb89a577336b567d
orapai <ivy.orapa.amodia@gmail.com>
2023-01-26 18:54:10 -0800

    made changes

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment()
+    {
+    }
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+    public string GetSummary()
+    {
+        return _studentName + " - " + _topic;
+    }
+}
