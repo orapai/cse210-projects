84694035c17655454c6831a50ddb2957cd98b9a2
orapai <ivy.orapa.amodia@gmail.com>
2023-02-06 01:53:37 -0800

    made changes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> list = new List<Shape>();
+        Square square = new Square("green", 2.5);
+        list.Add(square);
+        Rectangle rectangle = new Rectangle("red", 4.75, 3);
+        list.Add(rectangle);
+        Circle circle = new Circle("blue", 5.25);
+        list.Add(circle);
+
+        foreach (Shape shape in list)
+        {
+            Console.WriteLine($"Shape {shape.GetColor()} has an area of {shape.GetArea()}.");
+        }   
     }
 }
